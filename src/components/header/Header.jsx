36980bee5c3193ba7751e466e2cc9cf2aea3a704
W4veN <karolwajs55@gmail.com>
2022-08-10 20:25:52 +0200

    styles changed, react icons added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,17 @@ import './header.css';
 const Header= () => {
     return (
         <header>
+            <h5> portfolio.</h5>
             <div className="container header__container">
-                <h5> portfolio.</h5>
                 <h1> I'm Karol Wajs </h1>
+                <p> And ... I am Quality Engineer </p>
+                <p> Based in the beautiful city of Bali, Indonesia. </p>
+                <p> I am currently exploring the UI/UX design, </p>
+                <p> Tailwind-css and NextJS. </p>
+                <p> I am either smiling or looking super serious.</p>
+                <p> If it's the latter one, I am probably thinking </p>
+                <p> about food. </p>
+
             </div>
         </header>
     )
