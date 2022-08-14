e30f3946c78bc8c11a3ec495dd86cbd3c32af7d2
W4veN <karolwajs55@gmail.com>
2022-08-14 16:21:38 +0200

    sticky nav component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Experience from "./components/experience/Experience";
 import Resume from "./components/resume/Resume";
 import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
 
 const App= () => {
     return (
         <>
             <Header />
+            <Nav />
             <About />
             <Experience />
             <Resume />
