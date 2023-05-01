d1c4c03243b1c87e8712b875fa77fd27c2e8d279
W4veN <karolwajs55@gmail.com>
2023-05-01 17:41:53 +0200

    remove experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
 import Resume from "./components/resume/Resume";
 import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
@@ -12,7 +11,6 @@ const App= () => {
             <Header />
             <Nav />
             <About />
-            <Experience />
             <Resume />
             <Contact />
         </>
